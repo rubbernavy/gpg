070155ebe9bdb4e850ec40a5c04beb522f1ed82c
Emory Merryman <emory.merryman@deciphernow.com>
2016-12-25 18:38:01 -0500

    rework

--- create_dot_gnupg_volume.sh
+++ create_dot_gnupg_volume.sh
@@ -2,8 +2,10 @@
 
 dnf update --assumeyes &&
   dnf install --assumeyes gnupg gnupg2 &&
-  gpg --import /root/secret.key &&
-  gpg2 --import /root/secret.key &&
+  gpg --import /root/public.key &&
+  gpg2 --import /root/public.key &&
+  gpg --import /root/private.key &&
+  gpg2 --import /root/private.key &&
   gpg --import-ownertrust /root/owner.trust &&
   gpg2 --import-ownertrust /root/owner.trust &&
   true
