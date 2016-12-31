49d1f62fe0fd298c6b160a57fd68c19f2c464cfb
Emory Merryman <emory.merryman@deciphernow.com>
2016-12-31 08:27:59 -0500

    redo provisioning logic

--- provisioner.sh
+++ provisioner.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-DOT_GNUPG=$(docker volume create --name dot_gnupg) &&
-dnf update --assumeyes &&
-  docker \
-    run \
-    --interactive \
-    --rm \
-    --volume /vagrant/create_dot_gnupg_volume.sh:/root/bin/create_dot_gnupg_volume.sh:ro \
-    --volume /vagrant/secret.key:/root/secret.key:ro \
-    --volume /vagrant/owner.trust:/root/owner.trust:ro \
-    --volume ${DOT_GNUPG}:/root/.ssh \
-    --workdir /root \
-    fedora:25 \
-    sh /root/bin/create_dot_gnupg_volume.sh &&
-  dnf update --assumeyes &&
-  dnf clean all &&
+/bin/sh/create_dot_gnupg_volume.sh &&
   true
