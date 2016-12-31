1be444e0646ef9a2a6d865f9d279f718a5538d3e
Emory Merryman <emory.merryman@deciphernow.com>
2016-12-31 08:05:00 -0500

    add boxing script

---
+++ box.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+COMMIT_ID=$(git rev-parse --verify HEAD) &&
+  WORK_DIR=$(mktemp -d) &&
+  vagrant destroy -f &&
+  vagrant provision &&
+  vagrant up &&
+  sleep 30s &&
+  vagrant halt &&
+  vagrant up &&
+  sleep 30s &&
+  vagrant halt &&
+  vagrant up &&
+  sleep 30s &&
+  vagrant halt &&
+  vagrant package --output ${WORK_DIR}/gpg.${COMMIT_ID}.box &&
+  vagrant box add --name gpg.${COMMIT_ID} ${WORK_DIR}/gpg.${COMMIT_ID}.box &&
+  vagrant halt &&
+  rm -rf ${WORK_DIR} &&
+  true
