51e96e55f7724fabac1123f5073ee51009346336
Emory Merryman <emory.merryman@deciphernow.com>
2016-12-31 12:14:30 -0500

    destroy after boxing

--- create_dot_gnupg_volume.sh
+++ create_dot_gnupg_volume.sh
@@ -28,31 +28,31 @@ DOT_GNUPG=$(docker volume create --name dot_gnupg) &&
     --volume /vagrant:/usr/local/src:ro \
     emorymerryman/gpg:0.0.0 \
     --import-ownertrust /usr/local/src/ownertrust.gpg.key &&
-    docker \
-      run \
-      --interactive \
-      --tty \
-      --detach \
-      --volume ${DOT_GNUPG}:/root/.gnupg \
-      --volume /vagrant:/usr/local/src:ro \
-      emorymerryman/gpg2:0.0.0 \
-      --import /usr/local/src/public.gpg2.key &&
-    docker \
-      run \
-      --interactive \
-      --tty \
-      --detach \
-      --volume ${DOT_GNUPG}:/root/.gnupg \
-      --volume /vagrant:/usr/local/src:ro \
-      emorymerryman/gpg2:0.0.0 \
-      --import /usr/local/src/private.gpg2.key &&
-    docker \
-      run \
-      --interactive \
-      --tty \
-      --detach \
-      --volume ${DOT_GNUPG}:/root/.gnupg \
-      --volume /vagrant:/usr/local/src:ro \
-      emorymerryman/gpg2:0.0.0 \
-      --import-ownertrust /usr/local/src/ownertrust.gpg2.key &&
-    true
+  docker \
+    run \
+    --interactive \
+    --tty \
+    --detach \
+    --volume ${DOT_GNUPG}:/root/.gnupg \
+    --volume /vagrant:/usr/local/src:ro \
+    emorymerryman/gpg2:0.0.0 \
+    --import /usr/local/src/public.gpg2.key &&
+  docker \
+    run \
+    --interactive \
+    --tty \
+    --detach \
+    --volume ${DOT_GNUPG}:/root/.gnupg \
+    --volume /vagrant:/usr/local/src:ro \
+    emorymerryman/gpg2:0.0.0 \
+    --import /usr/local/src/private.gpg2.key &&
+  docker \
+    run \
+    --interactive \
+    --tty \
+    --detach \
+    --volume ${DOT_GNUPG}:/root/.gnupg \
+    --volume /vagrant:/usr/local/src:ro \
+    emorymerryman/gpg2:0.0.0 \
+    --import-ownertrust /usr/local/src/ownertrust.gpg2.key &&
+  true
