20cfd120ea9d68847da0c93272c92b5262fd0bda
Emory Merryman <emory.merryman@deciphernow.com>
2017-01-07 16:28:32 -0500

    some progress ... yes

--- create_dot_gnupg_volume.sh
+++ create_dot_gnupg_volume.sh
@@ -143,14 +143,6 @@ volume(){
       /usr/bin/sh /vagrant/injections/sbin/pass.sh ${@} &&
       true
   } &&
-  git(){
-    export BIN=${PASS_BIN} &&
-      export SBIN=${PASS_SBIN} &&
-      export SUDO=${PASS_SUDO} &&
-      export WORK=${DOT_PASSWORD_STORE} &&
-      /usr/bin/sh /vagrant/injections/sbin/git.sh ${@} &&
-      true
-  } &&
   echo allow-loopback-pinentry | docker \
     run \
     --interactive \
@@ -164,75 +156,10 @@ volume(){
   gpg --import-ownertrust --no-tty ownertrust.gpg.key &&
   (gpg --list-keys || true) &&
   pass init D65D3F8C &&
-  echo DOIN STUF ............................... &&
-
-    docker \
-      run \
-      --interactive \
-      --rm \
-      --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-      --volume ${SBIN}:/usr/local/sbin:ro \
-      --volume ${BIN}:/usr/local/bin:ro \
-      --volume ${SUDO}:/etc/sudoers.d:ro \
-      --volume ${DOT_PASSWORD_STORE}:/home/user/.password-store \
-      emorymerryman/base:0.0.6 \
-      which pass git &&
-
-              docker \
-                run \
-                --interactive \
-                --rm \
-                --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-                --volume ${SBIN}:/usr/local/sbin:ro \
-                --volume ${BIN}:/usr/local/bin:ro \
-                --volume ${SUDO}:/etc/sudoers.d:ro \
-                --volume ${DOT_PASSWORD_STORE}:/home/user/.password-store \
-                --user user \
-                emorymerryman/base:0.0.6 \
-                which pass git &&
-
-                        docker \
-                          run \
-                          --interactive \
-                          --rm \
-                          --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-                          --volume ${SBIN}:/usr/local/sbin:ro \
-                          --volume ${BIN}:/usr/local/bin:ro \
-                          --volume ${SUDO}:/etc/sudoers.d:ro \
-                          --volume ${DOT_PASSWORD_STORE}:/home/user/.password-store \
-                          --user user \
-                          emorymerryman/base:0.0.6 \
-                          env &&
-
-
-  pass git init &&
-  echo THERE .................................. &&
   pass git config user.name "Emory Merryman" &&
   pass git config user.email "emory.merryman+$(uuidgen)@gmail.com" &&
   pass git remote add origin https://github.com/desertedscorpion/passwordstore.git &&
   pass git fetch origin master &&
   pass git checkout origin/master &&
   pass show &&
-  docker pull emorymerryman/pass:0.6.0 &&
-  docker pull emorymerryman/ssh:0.0.1 &&
-  docker pull emorymerryman/tree:0.0.2 &&
-  sed \
-    -e "s#\${USR_BIN_DIR}#${PASS_BIN_DIR}#" \
-    -e "s#\${SUDO_DIR}#${PASS_SUDO_DIR}#" \
-    -e "s#\${DOT_SSH}#${DOT_SSH}#" \
-    -e "s#\${DOT_GNUPG}#${DOT_GNUPG}#" \
-    -e "s#\${PASS_STORE}#${PASS_STORE}#" \
-    -e "w/usr/local/bin/pass" \
-    /vagrant/injections/pass.sh &&
-  chmod 0555 /usr/local/bin/pass &&
-  sed \
-    -e "s#\${USR_BIN_DIR}#${PASS_BIN_DIR}#" \
-    -e "s#\${SUDO_DIR}#${PASS_SUDO_DIR}#" \
-    -e "s#\${DOT_SSH}#${DOT_SSH}#" \
-    -e "s#\${DOT_GNUPG}#${DOT_GNUPG}#" \
-    -e "s#\${SRC_DIR}#${DOT_GNUPG}#" \
-    -e "s#\${PASS_STORE}#${PASS_STORE}#" \
-    -e "w/usr/local/bin/gpg" \
-    /vagrant/injections/gpg.sh &&
-  chmod 0555 /usr/local/bin/gpg &&
   true
