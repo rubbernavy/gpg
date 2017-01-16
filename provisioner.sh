def8e4b5a0614644dc3281ad18b9bd71eb1c7f5e
Emory Merryman <emory.merryman@deciphernow.com>
2017-01-16 08:17:44 -0500

    forget about docker

--- provisioner.sh
+++ provisioner.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-/bin/sh /vagrant/create_dot_gnupg_volume.sh &&
+dnf update --assumeyes &&
+  dnf install --assumeyes git &&
+  WORK_DIR=$(mktemp -d) &&
+  git -C ${WORK_DIR} init &&
+  git -C ${WORK_DIR} remote add origin https://github.com/furiousfox/password-store.git &&
+  git -C ${WORK_DIR} fetch origin &&
+  git -C ${WORK_DIR} checkout tags/1.6.5 &&
+  make --directory ${WORK_DIR} install &&
+  su --login vagrant --command /vagrant/vagrant.sh &&
+  dnf update --assumeyes &&
   true
