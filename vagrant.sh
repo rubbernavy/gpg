8931dda1c2b7528fec7870122539950d7863cd5a
Emory Merryman <emory.merryman@deciphernow.com>
2017-01-16 11:53:45 -0500

    replace HOME

--- vagrant.sh
+++ vagrant.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-ssh-keygen -f ${HOME}/.ssh/id_rsa -P "" &&
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -P "" &&
   curl \
   -H "Authorization: token $(cat /vagrant/github.oauth.token)" \
   --request POST \
-  --data "{\"title\": \"Vagrant $(uuidgen)\", \"key\": \"$(cat ${HOME}/.ssh/id_rsa.pub)\"}" \
-  cat /vagrant/config > ${HOME}/.ssh/config &&
-  chmod 0600 ${HOME}/.ssh/config &&
+  --data "{\"title\": \"Vagrant $(uuidgen)\", \"key\": \"$(cat /home/vagrant/.ssh/id_rsa.pub)\"}" \
+  cat /vagrant/config > /home/vagrant/.ssh/config &&
+  chmod 0600 /home/vagrant/.ssh/config &&
   https://api.github.com/user/keys &&
   gpg --import /vagrant/private.gpg.key &&
   gpg --import /vagrant/public.gpg.key &&
@@ -17,7 +17,7 @@ ssh-keygen -f ${HOME}/.ssh/id_rsa -P "" &&
   pass git remote add origin git@github.com:desertedscorpion/passwordstore.git &&
   pass git fetch origin master &&
   pass git checkout master &&
-  cat /vagrant/post-commit.sh > ${HOME}/.password-store/.git/hooks/post-commit &&
-  chmod 0500 ${HOME}/.password-store/.git/hooks/post-commit &&
+  cat /vagrant/post-commit.sh > /home/vagrant/.password-store/.git/hooks/post-commit &&
+  chmod 0500 /home/vagrant/.password-store/.git/hooks/post-commit &&
   pass show &&
   true
