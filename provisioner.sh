830065d4919da986a14fa4ce3d6387e7b94d2d07
Emory Merryman <emory.merryman@deciphernow.com>
2017-01-16 11:52:45 -0500

    add atom

--- provisioner.sh
+++ provisioner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dnf update --assumeyes &&
-  dnf install --assumeyes git xclip pwgen curl &&
+  dnf install --assumeyes git xclip pwgen curl wget &&
   WORK_DIR=$(mktemp -d) &&
   git -C ${WORK_DIR} init &&
   git -C ${WORK_DIR} remote add origin https://github.com/furiousfox/password-store.git &&
@@ -9,5 +9,8 @@ dnf update --assumeyes &&
   git -C ${WORK_DIR} checkout tags/1.6.5 &&
   make --directory ${WORK_DIR} install &&
   su --login vagrant --command "/usr/bin/sh /vagrant/vagrant.sh" &&
+  wget --output-document ${WORK_DIR}/atom.x86_64.rpm https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm &&
+  dnf install ${WORK_DIR}/atom.x86_64.rpm &&
+  rm --recursive --force ${WORK_DIR} &&
   dnf update --assumeyes &&
   true
